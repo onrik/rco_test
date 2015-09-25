2eb59ecffa61e0d32e7548957f3a44fed5fafc79
Andrey <and@rey.im>
2015-09-25 09:29:51 +0600

    Implement uploading status progress.

--- server.py
+++ server.py
@@ -8,6 +8,8 @@ import uuid
 import asyncio
 from aiohttp import web
 
+status = {'loaded': 0}
+
 
 @asyncio.coroutine
 def index_handler(request):
@@ -16,10 +18,26 @@ def index_handler(request):
 
 @asyncio.coroutine
 def upload_handler(request):
-    data = yield from request.content.read()
+    length = int(request.headers['CONTENT-LENGTH'])
+    
+    buff = io.BytesIO()
+    if request.content.is_eof():
+        data = yield from request.content.read()
+        buff.write(data)
+        status['loaded'] = 100
+    else:
+        total = 0
+        chunk_size = 100
+        while not request.content.is_eof():
+            data = yield from request.content.read(chunk_size)
+            buff.write(data)
+            total += chunk_size
+            status['loaded'] = int((total * 100) / length)
+
+    buff.seek(0)
     _, pdict = cgi.parse_header(request.headers['CONTENT-TYPE'])
     pdict['boundary'] = pdict['boundary'].encode()
-    multipart = cgi.parse_multipart(io.BytesIO(data), pdict)
+    multipart = cgi.parse_multipart(buff, pdict)
 
     filename = uuid.uuid4().hex
     with open('static/' + filename, 'wb') as f:
@@ -31,10 +49,16 @@ def upload_handler(request):
 
 
 @asyncio.coroutine
+def status_handler(request):
+    return web.Response(body=json.dumps(status).encode())
+
+
+@asyncio.coroutine
 def init(loop, port):
     app = web.Application(loop=loop)
     app.router.add_route('GET', '/', index_handler)
     app.router.add_route('POST', '/upload', upload_handler)
+    app.router.add_route('GET', '/status', status_handler)
     app.router.add_static('/static', './static/')
 
     server = yield from loop.create_server(app.make_handler(), '0.0.0.0', port)
